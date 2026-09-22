91732b8ce32d4429e1944bb0dfe2963a3e317c36
Marvin Sakali <marvinsakali09@gamil.com>
2026-09-22 10:54:12 +0300

    add new feature

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import NavBar from '../components/NavBar'
 import HeroSection from '../components/HeroSection'
-import FeaturedSection from '../components/FeaturedSection'
 
 const Home = () => {
   const [showLogin, setShowLogin] = useState(false)
@@ -9,7 +8,6 @@ const Home = () => {
     <div>
         <NavBar setShowLogin={setShowLogin}/>
         <HeroSection/>
-        <FeaturedSection/>
     </div>
   )
 }
